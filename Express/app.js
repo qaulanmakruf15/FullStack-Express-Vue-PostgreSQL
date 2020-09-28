04d9357924f16772d191d6c6045c69363b2b804c
qaulanmakruf15 <qaulanmakruf15@gmail.com>
2020-09-28 15:39:22 +0700

    add log node.js

--- Express/app.js
+++ Express/app.js
@@ -6,6 +6,8 @@ const cors = require('cors')
 const routerNavigation = require('./src/main')
 const port = 3000;
 const app = express();
+const fs = require('fs')
+const path = require('path')
 
 app.use(cors()) 
 app.use(bodyParser.json());
@@ -14,6 +16,9 @@ app.use(morgan("dev"))
 
 app.use("/api", routerNavigation)
 app.use(express.static("uploads")) 
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'project.log'), { flags: 'a' })
+app.use(morgan('common', { stream: accessLogStream }))
+app.use(morgan('dev'))
 
 app.listen(port, () => {
     console.log(`Service running on port ${port}`)
